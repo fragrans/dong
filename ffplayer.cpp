b1d4dcffde20e5ea21df3c3c5299524c2ce2fef7
farseer <farseer@hp.compal.com>
2009-07-06 16:23:01 +0800

    Yes, this is farseer.

--- ffplayer.cpp
+++ ffplayer.cpp
@@ -5,6 +5,7 @@ using namespace std;
 /* master branch */
 /* in experimental branch */
 /* now merged" */
+/* now farseer changed this file */
 int main()
 {
     cout << "ffplayer is running and over!" << endl;
